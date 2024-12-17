3c01d6e93a8053b4e7749792fb36dc798b12315e
dashka-bat <dushkab02@gmail.com>
2024-12-17 15:32:59 +0800

    vercel fixed

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("da1a3ceb58c94392")
+/******/ 		__webpack_require__.h = () => ("6f0270cdb7b3aafe")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("23f5df0f0d81c00e")
+/******/ 		__webpack_require__.h = () => ("797afc04a318784e")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
